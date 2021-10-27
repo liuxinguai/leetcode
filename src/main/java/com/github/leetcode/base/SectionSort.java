7d8da391c83aff6fbd0382e2514704b2e1d7b614
xinguai.liu <xinguai.liu@yeego.com>
2021-10-27 15:27:35 +0800

    first commit

---
+++ src/main/java/com/github/leetcode/base/SectionSort.java
@@ -0,0 +1,21 @@
+package com.github.leetcode.base;
+
+public class SectionSort {
+
+    public void sort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
